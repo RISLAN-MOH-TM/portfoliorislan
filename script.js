2a559d950dc0c817bdaa42ee4797daa9b9ded00f
RISLAN-MOH-TM <rislanmohammed151@gmail.com>
2025-07-08 10:10:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function validateForm() {
+    let name = document.getElementById("name").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let message = document.getElementById("message").value.trim();
+
+    if (name === "" || email === "" || message === "") {
+        alert("All fields are required!");
+        return false;
+    }
+    alert("Message sent successfully!");
+    return true;
+}
